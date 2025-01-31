ec9256d29822f06e5aa0668c4a2ae86a21ff15c7
gregorymohacsi <gregory.mohacsi@gmail.com>
2025-01-31 22:56:00 +1300

    git ignore

---
+++ backend/controllers/goalsController.js
@@ -0,0 +1,11 @@
+
+//@desc   Get Goals
+//@route  GET /api/goals
+//@access  Private
+const getGoals = (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+}
+
+module.exports = {
+    getGoals, 
+}
